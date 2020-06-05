3a939d36663c0760f8751d082eb959bafc5bc2c9
Suwadith <suwadith.srithar@gmail.com>
2020-06-06 04:05:39 +0530

    update

---
+++ src/com/suwadith/FizzBuzz.java
@@ -0,0 +1,26 @@
+package com.suwadith;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Number: ");
+        int number = sc.nextInt();
+
+        if ( (number % 5) == 0 && (number % 3 == 0) ) {
+            System.out.println("FizzBuzz");
+        } else if ((number % 5) == 0) {
+            System.out.println("Fizz");
+        } else if ((number % 3) == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
+
+    }
+
+}
